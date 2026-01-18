e7fabecc4138404fff0f0c0b3157ab5b0166badf
iuuukhueeee <nguyenducthien9@gmail.com>
2026-01-18 12:38:25 +0700

    feat: add media routes

--- backend/main.ts
+++ backend/main.ts
@@ -2,6 +2,7 @@ import { Queue } from 'bullmq';
 
 import express from 'express'
 import cors from 'cors'
+import { prisma } from './lib/prisma';
 
 const app = express()
 app.use(cors())
@@ -22,4 +23,16 @@ app.post('/api/scrape', async (req, res) => {
   await scrapeQueue.addBulk(jobs);
 
   res.status(202).json({ message: `${urls.length} tasks queued.` });
+})
+
+app.get('/api/media', async (req, res) => {
+  const mediaType = req.query.media_type as string | undefined
+
+  const where = mediaType ? { media_type: mediaType } : {}
+
+  const data = await prisma.media.findMany({
+    where
+  })
+
+  return res.status(200).json(data)
 })
